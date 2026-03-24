6951478a6826766d6c80cb455d5b543c9254dbae
ancii <anjagrudnik04@gmail.com>
2026-03-24 15:08:13 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import sys
 import os
 
-sys.path.append(os.path.join(os.path.dirname(__file__), "preobdelava_slik"))
+sys.path.append(os.path.join(os.path.dirname(__file__), "preprosta_predobdelava_slik", "preobdelava_slik"))
 
 import test
